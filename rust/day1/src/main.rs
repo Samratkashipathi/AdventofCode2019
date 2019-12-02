80151f96efc5f3f2a9a9b48aa097b45efa02b0f9
Samrat K S <samratkashipathi1@gmail.com>
2019-12-02 21:24:59 +0530

    Changed implementation

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -4,13 +4,11 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let file = fs::File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
-    let mut total_fuel: f64 = 0.0;
-
-    for item in reader.lines() {
-        let mass: i64 = item.unwrap().parse().unwrap();
-        let fuel = (mass as f64 / 3.0).floor() - 2.0;
-        total_fuel = total_fuel + fuel
-    }
+    let mut total_fuel: f64 = reader.lines()
+        .map(|x| x.unwrap().parse().unwrap())
+        .map(|x: i64| x as f64)
+        .map(|x| (x / 3.0).floor())
+        .map(|x| x - 2.0).sum();
 
     println!("{:#?}", total_fuel)
 }
